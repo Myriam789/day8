6b99e168c86f03d0610776c213e49368f40881f7
Myriam789 <myriam.sall@epitech.digital>
2024-10-09 22:38:46 +0200

    ex_02.js

---
+++ ex_02.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+    <title>Carte Epitech digital school</title>
+    <link rel="stylesheet" href="https://leafletjs.com/examples/quick-start/">
+</head>
+<body>
+ <div id = map >
+   
+
+ </var>
+ </div>
+</body>
