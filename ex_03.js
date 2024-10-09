3e45d5aaea38c3be0102ccb76ecba5641b6972d5
Myriam789 <myriam.sall@epitech.digital>
2024-10-09 22:56:16 +0200

    ex_03.js

--- ex_03.js
+++ ex_03.js
@@ -1,12 +1,24 @@
-!DOCTYPE html>
-<head>
-    <title>Carte Epitech digital school</title>
-    <link rel="stylesheet" href="https://leafletjs.com/examples/quick-start/">
+<!DOCTYPE html>
+<html>
+    <head>
+     <title>click button</title>
+
+     <link rel="stylesheet" href="styles.css">
+     
 </head>
 <body>
- <div id = map >
-   
+    <div class="element images "> </div>
+    <button on click></button>
 
- </var>
- </div>
+    <div class="element square"></div>
+    <button on click></button>
+
+    <div class="element sound"></div>
+    <button on click></button>
+
+    <div class="element video"></div>
+    <buttonon click></buttonon>
+
+    <div class="element button"></div>
 </body>
+</html>
